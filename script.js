68c43d70ffdf17658c29ba1200b2faf80d991745
junrainer4 <junrainer4@gmail.com>
2025-12-05 21:29:15 +0800

    add homecode

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleFAQ() {
+    var x = document.getElementById("faqbox");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
